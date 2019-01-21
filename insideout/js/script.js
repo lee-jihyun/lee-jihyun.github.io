9caef1ddc12eb7893b6eb42d58896df75a1cf121
leejihyun <www.jihyunlee.com@gmail.com>
2019-01-21 19:44:18 +0900

    no message

--- insideout/js/script.js
+++
@@ -1,9 +0,0 @@
-// nav
-
-// $('.button').click(function(){
-// 	var color = $(this).parent().attr("class")
-// 	var body = $("#canvas").attr("class")
-// 	console.log(body)
-//    $("#canvas").attr('class', color + "-background");
-// });
-
